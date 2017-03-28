aabcd65fd9257ec19d2f7c74932a5c38363651e8
cubemaster21 <cubemaster21@comcast.net>
2017-03-27 21:13:58 -0400

    Fixed Screen not clearing on every frame.

--- com/toasted/momentus/Momentus.java
+++ com/toasted/momentus/Momentus.java
@@ -2,6 +2,7 @@ package com.toasted.momentus;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -46,7 +47,8 @@ public class Momentus extends ApplicationAdapter{
 	@Override
 	public void render () {
 		currentScreen.update(Gdx.graphics.getDeltaTime());
-		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		currentScreen.draw();
 		
 		if(nextScreen != null){
